ac082a263944ad3d7cb55fc01161fac80cd31a51
smolnik <smolnik@wp.pl>
2014-07-27 22:12:18 +0200

    ExtractionClient welcomed on board.

---
+++ src/main/java/net/adamsmolnik/client/ExtractionClient.java
@@ -0,0 +1,25 @@
+package net.adamsmolnik.client;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.Response;
+import net.adamsmolnik.model.extraction.ExtractionRequest;
+import net.adamsmolnik.model.extraction.ExtractionResponse;
+
+/**
+ * @author ASmolnik
+ *
+ */
+public class ExtractionClient {
+
+    public static void main(String[] args) throws Exception {
+        Client client = ClientBuilder.newClient();
+        Entity<ExtractionRequest> request = Entity.json(new ExtractionRequest("myfolder/awsugpl.zip", "zip"));
+        System.out.println(Entity.json(request).toString());
+        Response response = client.target("http://localhost:8080/extraction-service/es/extract").request().post(request);
+        ExtractionResponse responseObject = response.readEntity(ExtractionResponse.class);
+        System.out.println(responseObject);
+    }
+
+}
